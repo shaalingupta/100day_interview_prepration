2472e26b7127a7660b165d17cb7e12914c08478f
shaalingupta <Shalin.Gupta@Honeywell.com>
2020-01-24 20:32:43 +0530

    Day1

---
+++ data_structure_algorithm/Day1/TwoSum.java
@@ -0,0 +1,17 @@
+/**
+ * Two Sum:
+ * 
+ * Given An Array of Integers, return indices of the two numbers such that
+ * they add up to a specific target.
+ * 
+ * 
+ * Example:: 
+ * Given nums = [2, 7, 11, 15],
+ * target = 9,
+ * 
+ * Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
+ *
+ * 
+ */
+
+ 
